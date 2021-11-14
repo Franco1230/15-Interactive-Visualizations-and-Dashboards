c057f3fb742b123a322109818361613e9d5b8913
Franco1230 <franco.hfh1230@gmail.com>
2021-11-14 15:08:31 +1100

    changed color

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,86 +1,83 @@
 function init() {
   d3.json('samples.json').then(data => {
-      console.log(data);
-      let id = "940"
-      // filter samples to element 940
-      let filtered = data.samples.filter(sample => sample.id === id);
-      let y = filtered.map(otus => otus.otu_ids);
-      console.log(filtered);
-      console.log("This is the mapped otu ids for ID #940: ",y[0].slice(0,10));
-      console.log(filtered[0].otu_ids);
-      //  get data for plots
-      let sample = data.samples.filter(sample => sample.id === id);
-      let x_bubble = sample[0].otu_ids;
-      let y_bubble = sample[0].sample_values;
-      let y_bar = y[0].slice(0,10).map(String);
-      y_bar = y_bar.map(el => "OTU "+el)
-      console.log(y_bar);
-      let x_bar = sample[0].sample_values.slice(0,10);
-      console.log(x_bar);
-      let droplist = data.names;
-      var demographic = data.metadata[0];
-      console.log(d3.keys(demographic));
-      
-      // build initial hbar plot
-      var trace1 = {
-          x: x_bar.reverse(),
-          y: y_bar.reverse(),
-          type: 'bar',
-          orientation: 'h'
-      };
-      
-      var plotdata = [trace1];
-  
-      Plotly.newPlot("bar", plotdata);
-  
-      // build initial bubble plot
-      var trace2 = {
-          x: x_bubble,
-          y: y_bubble,
-          mode: 'markers',
-          marker: {
-              size: sample[0].sample_values,
-              color: sample[0].sample_values,
-              colorscale: [[0, 'rgb(0, 0, 0)'], [1, 'rgb(0, 255, 255)']]
-          },
-          text: sample[0].otu_labels
-      };
-      
-      var plotdata2 = [trace2];
-  
-      Plotly.newPlot("bubble", plotdata2);
+    console.log(data);
+    let id = "940"
+    
+    // Filter samples to element 940
+    let filtered = data.samples.filter(sample => sample.id === id);
+    let y = filtered.map(otus => otus.otu_ids);
+    console.log("Mapped otu ids for ID #940: ", y[0].slice(0, 10));
+        
+    // Get data for bubble graph
+    let sample = data.samples.filter(sample => sample.id === id);
+    let x_bubble = sample[0].otu_ids;
+    let y_bubble = sample[0].sample_values;
+    
 
-      // build gauge plot
-      var gdata = [
-          {
-              domain: { x: [0, 1], y: [0, 1] },
-              value: demographic.wfreq,
-              title: { text: "Belly Button Washing Frequency" },
-              type: "indicator",
-              gauge: {
-                  bar: { color: "darkgrey" }
-              },
-              mode: "gauge+number"
-          }
-      ];
-      
+    // Get data for bar graph
+    let x_bar = sample[0].sample_values.slice(0, 10);
+    console.log(x_bar);
+    let y_bar = y[0].slice(0,10).map(String);
+    y_bar = y_bar.map(el => "OTU " + el)
+    console.log(y_bar);
+
+    // Fill in the dropdown list
+    let dropdown = data.names;
+    var demographic = data.metadata[0];
+    console.log(d3.keys(demographic));
+    
+    // Create horizontal bar graph
+    var trace1 = {
+      x: x_bar.reverse(),
+      y: y_bar.reverse(),
+      type: 'bar',
+      orientation: 'h'
+    };
+    var plotdata = [trace1];
+    Plotly.newPlot("bar", plotdata);
+    
+    // Create bubble plot
+    var trace2 = {
+      x: x_bubble,
+      y: y_bubble,
+      mode: 'markers',
+      marker: {
+        size: sample[0].sample_values,
+        color: sample[0].sample_values,
+        colorscale: [[0, 'rgb(0, 0, 0)'], [1, 'rgb(255, 0, 255)']]
+      },
+      text: sample[0].otu_labels
+    };
+    
+    var plotdata2 = [trace2];
+    Plotly.newPlot("bubble", plotdata2);
+    
+    // Create gauge
+    var gdata = [{
+      domain: {x: [0, 1], y: [0, 1]},
+      value: demographic.wfreq,
+      title: {text: "Belly Button Washing Frequency"},
+      type: "indicator",
+      gauge: {
+        bar: {color: "charcoal"}
+      },
+      mode: "gauge+number"
+    }];
+
       var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
 
       Plotly.newPlot('gauge', gdata, layout);
       // build dropdown list
       const menu = d3.select("#selDataset");
   
-      droplist.forEach(item => {
+      dropdown.forEach(item => {
           menu.append("option").attr("value", item).text(item);
       })
       // build metadata card
-      // got solution here: 
-      // https://stackoverflow.com/questions/37673454/javascript-iterate-key-value-from-json
       const meta = d3.select("#sample-metadata");
       Object.keys(demographic).forEach((k) => {
-          // meta.append("p").attr("class", "card-text").text(d3.keys(elem));
-          console.log(k, demographic[k]);
-          meta.append("p").attr("class", "card-text").text(`${k}: ${demographic[k]}`);
+        console.log(k, demographic[k]);
+        meta.append("p").attr("class", "card-text").text(`${k}: ${demographic[k]}`);
       });
 
       function optionChanged() {
