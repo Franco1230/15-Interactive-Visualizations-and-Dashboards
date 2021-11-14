37bd5d873fe20b55652bfde5a5b104a84c00f66e
Franco1230 <franco.hfh1230@gmail.com>
2021-11-14 17:13:13 +1100

    changed colors for graphs

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -53,7 +53,7 @@ function init() {
       marker: {
         size: sample[0].sample_values,
         color: sample[0].sample_values,
-        colorscale: [[0, "rgb(90, 100, 110)"], [1, "rgb(40, 0, 50)"]]
+        colorscale: [[0, "rgb(90, 100, 110)"], [1, "rgb(50, 0, 60)"]]
       },
       // text: sample[0].otu_labels
     };
